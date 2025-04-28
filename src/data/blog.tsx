7403c6847a83e297eb5aaf98029e9406c730c42b
Anshul Kanswal <anshulkanswal01@gmail.com>
2025-04-28 23:30:17 +0530

    Add blog data file with initial structure

---
+++ src/data/blog.tsx
@@ -0,0 +1,12 @@
+export const BLOG_DATA = {
+  title: "Blog",
+  description: "My thoughts and experiences",
+  posts: [
+    {
+      title: "Coming Soon",
+      description: "Blog posts are coming soon...",
+      date: "2024",
+      link: "#",
+    },
+  ],
+} as const; 
